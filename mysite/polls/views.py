74f5599e5b25f4d7ee85dcb415e0046c7c8b7ff2
Dave Vigliotti <dave.vigliotti@gmail.com>
2021-09-02 13:07:28 -0400

    Revert "Revert "testing PullRequest""
    This reverts commit 60c232ff9c20aa53683c1ec679917ec9e322063b.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,3 +13,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+    ok, this is a change requiring a pull request
